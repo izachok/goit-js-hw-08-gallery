487eea1d37259202ba309e077b84eba586c15143
Olga Tiutiunnyk <izachok@gmail.com>
2021-06-26 20:05:46 +0300

    add finished base variant hw

---
+++ js/gallery.js
@@ -0,0 +1,106 @@
+// Создание и рендер разметки по массиву данных galleryItems из app.js и предоставленному шаблону.
+// Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
+// Открытие модального окна по клику на элементе галереи.
+// Подмена значения атрибута src элемента img.lightbox__image.
+// Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"].
+// Очистка значения атрибута src элемента img.lightbox__image. Это необходимо для того, чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее.
+
+import { galleryItems } from './app.js';
+
+const galleryContainer = document.querySelector('.js-gallery');
+const modalCloseBtn = document.querySelector('[data-action="close-lightbox"]');
+const modalRef = document.querySelector('.lightbox');
+const modalImgRef = modalRef.querySelector('.lightbox__image');
+
+let isModalOpen = false;
+
+galleryContainer.innerHTML = generateGalleryMarkup(galleryItems);
+addListeners();
+
+function generateGalleryMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="${original}"
+  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`,
+    )
+    .join('');
+}
+
+function addListeners() {
+  galleryContainer.addEventListener('click', onGalleryClick);
+  modalCloseBtn.addEventListener('click', closeModal);
+  modalRef
+    .querySelector('.lightbox__overlay')
+    .addEventListener('click', closeModal);
+  window.addEventListener('keydown', onModalKeydown);
+}
+
+function onGalleryClick(e) {
+  const element = e.target;
+  if (!element.classList.contains('gallery__image')) return;
+  e.preventDefault();
+
+  const src = element.dataset?.source;
+  openModal(src);
+}
+
+function onModalKeydown(event) {
+  if (!isModalOpen) return;
+
+  switch (event.code) {
+    case 'Escape':
+      closeModal();
+      break;
+    case 'ArrowRight':
+      nextGallerySrc();
+      break;
+    case 'ArrowLeft':
+      prevGallerySrc();
+      break;
+  }
+}
+
+function getCurrentGallerySrcIndex() {
+  return galleryItems.findIndex(item => item.original === modalImgRef.src);
+}
+
+function nextGallerySrc() {
+  const nextIndex = getCurrentGallerySrcIndex() + 1;
+  if (nextIndex < galleryItems.length)
+    setModalImgSrc(galleryItems[nextIndex].original);
+}
+
+function prevGallerySrc() {
+  const prevIndex = getCurrentGallerySrcIndex() - 1;
+  if (prevIndex >= 0) setModalImgSrc(galleryItems[prevIndex].original);
+}
+
+function setModalImgSrc(src) {
+  modalImgRef.src = src;
+}
+
+function openModal(src) {
+  document.body.classList.add('modal-open');
+  modalRef.classList.add('is-open');
+  isModalOpen = true;
+  setModalImgSrc(src);
+}
+
+function closeModal() {
+  document.body.classList.remove('modal-open');
+  modalRef.classList.remove('is-open');
+  isModalOpen = false;
+  setModalImgSrc('');
+}
